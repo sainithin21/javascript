194ae47cfcfda301b4961fb90ae6a57caeb8ee01
sainithin21 <136963264+sainithin21@users.noreply.github.com>
2025-08-15 07:48:33 +0000

    cont..

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -36,6 +36,42 @@ let strVal = String(num)
 console.log(strVal);
 console.log(typeof strVal);
 
+// *************** operations ******************
+
+let val = 3
+let negVal = -val
+console.log(negVal);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(3/2);
+console.log(3**2);
+console.log(3%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+2+3+"12");
+console.log(1+2+3+"4"+5+1);
+console.log(+true);
+console.log(+"");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
