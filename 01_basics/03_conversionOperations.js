d852f46523b83346a37e3634e04932f7b7634921
sainithin21 <136963264+sainithin21@users.noreply.github.com>
2025-08-15 07:14:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,45 @@
+let score = 33
+console.log(typeof(score));
+console.log(typeof score);
+
+let value1 = '33'
+console.log(typeof value1);
+let valueInNumber1 = Number(value1)
+console.log(valueInNumber1);
+
+let value2 = '33abc'
+let valueInNumber2 = Number(value2)
+console.log(valueInNumber2);
+console.log(typeof valueInNumber2);
+console.log(typeof NaN);
+
+let score2 = null // try with true and false
+console.log(typeof score2);
+let scoreInNumber2 = Number(score2)
+console.log(scoreInNumber2);
+
+let value3 = undefined;
+console.log(typeof value3);
+let valueInNumber3 = Number(value3)
+console.log(valueInNumber3);
+
+let isLoggedIn = 1;
+let isLoggedInVal = Boolean(isLoggedIn)
+console.log(isLoggedInVal);
+
+let isLoggedIn2 = ""; // empty string and non_empty string
+let isLoggedInVal2 = Boolean(isLoggedIn2)
+console.log(isLoggedInVal2);
+
+let num = 33
+let strVal = String(num)
+console.log(strVal);
+console.log(typeof strVal);
+
+
+
+
+
+
+
+
