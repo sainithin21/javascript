89ca8b43f1114c011a6c697aa1ad2c6b357a841e
sainithin21 <sainithin2326@gmail.com>
2025-08-20 17:26:42 +0530

    more on functions

---
+++ 04_Functions/02_functions.js
@@ -0,0 +1,36 @@
+function calculateCartPrice(...num1){
+   return num1
+}
+console.log(calculateCartPrice(200,300,400));
+
+function calculateCartPrice1(val1,val2,...num1){
+   return num1
+}
+console.log(calculateCartPrice1(200,300,400,500));
+
+const user = {
+    name:"sai nithin",
+    course_price:30000
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.name} and course price is ${user.course_price}`); 
+}
+
+handleObject(user)
+
+handleObject({
+    name:"sam",
+    course_price:25000
+})
+
+const myArr = [200,400,600,800]
+
+function returnSecondVal(arr){
+   return arr[1]
+}
+console.log(returnSecondVal(myArr));
+console.log(returnSecondVal([100,200,300,400,500]));
+
+
+
