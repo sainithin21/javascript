eb8d316f349bc62058f1bafa3ce1ea29428064a3
sainithin21 <sainithin2326@gmail.com>
2025-09-21 13:17:16 +0530

    variable scope

---
+++ scopes.js
@@ -0,0 +1,17 @@
+if (true){
+   let a = 10
+   const b = 20
+   var c = 30
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+// global scope is different in vscode and browser
+
+
